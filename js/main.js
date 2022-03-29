6fc5d059be5bc3ed31d7b644b2c3cdc9546fefef
adhscifair <84250312+adhscifair@users.noreply.github.com>
2022-03-29 17:08:09 -0400

    Update main.js
    more clean and neat!

--- js/main.js
+++ js/main.js
@@ -1,106 +1,100 @@
 //all the logic
-document.getElementById("question").innerHTML = "Click 'next question' for your questions to start appearing";
 let functionRun = 0;
 let rand = Math.floor(Math.random() * data.length);
 let randQuestionAndPoints = data[rand].questionAndPoints;
-console.log(randQuestionAndPoints)
 let questionNum = Math.floor(Math.random() * randQuestionAndPoints.length);
 let question = randQuestionAndPoints[questionNum].question;
 let answer = randQuestionAndPoints[questionNum].answer;
 let inputElement = document.getElementById("showAndHide");
 inputElement.style.display =  "none";
-let points = 0;
+let points = 0
+$(document).ready(function(){
+    $("#myModal").modal('show');
+});
 function wrongMessage (answer) {
-    let message = ` <div class="alert alert-danger" role="alert"> <b>You got the last question incorrect.<br/> The answer is: __ANSWER__ &#129300;</b></div>`;
+    answer = randQuestionAndPoints[questionNum].answer;
+    let message = ` <div class="alert alert-danger alert-dismissible fade show" role="alert"> <b>You got the last question incorrect.<br/> The answer is: __ANSWER__ &#129300;</b><button type="button" class="close" data-dismiss="alert" aria-label="Close">
+    <span aria-hidden="True">&times;</span>
+  </button></div>`;
     return message.replace("__ANSWER__", answer);
 }
 function rightMessage (points) {
-    let message = `<div class="alert alert-success" role="alert"><b> You got the last question correct!</b><br/><b> You have __POINTS__ point(s)! &#128077;</b></div>`;
+    let message = `<div class="alert alert-success alert-dismissible fade show" role="alert"><b> You got the last question correct!</b><br/><b> You have __POINTS__ point(s)! &#128077;</b><button type="button" class="close" data-dismiss="alert" aria-label="Close">
+    <span aria-hidden="True">&times;</span>
+  </button></div>`;
     return message.replace("__POINTS__", points);
+}
+
+function handleRightAnswer(questionNumber, points) {
+    pop()
+    let idName = "#el" + (questionNumber+1)
+    let aidName = idName + "a"
+    $(idName).removeClass("hidden-element")
+    $(idName).removeClass("element-no-transition")
+    $(idName).addClass("element")
+    $(aidName).removeClass("visible-span")
+    $(aidName).addClass("hidden-element")
+    $("#result").html(rightMessage(points))
+}
+
+function handleWrongAnswer(questionNumber, points) {
+    let idName = "#el" + (questionNumber+1)
+    let aidName = idName + "a"
+    $(idName).addClass("hidden-element")
+    $(idName).addClass("element-no-transition")
+    $(idName).removeClass("element")
+    $(aidName).removeClass("hidden-element")
+    $(aidName).addClass("visible-span")
+    $("#result").html(wrongMessage(points))
 }
 
 function nextQuestion () {
     inputElement.style.display = "block";
-    document.getElementById("next").innerHTML = "Continue";
+    $("#next").html("Continue");
     if(functionRun > 0){
-        if(document.getElementById("input").value.toLowerCase() == ""){
-            document.getElementById("result").innerHTML = `<div class="alert alert-warning" role="alert">You did not answer the last question!<br/> The answer is: ${answer}  &#9888;&#65039;`;
-        }else{
+        if($("#input").val() == ""){
+            $("#result").html(`<div class="alert alert-warning alert-dismissible fade show" role="alert">You did not answer the last question!<br/> The answer is: ${answer}  &#9888;&#65039;<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="True">&times;</span>
+          </button></div>`);
+        } else{
             if(isNaN(answer)){
-                if (answer.toLowerCase() == document.getElementById("input").value.toLowerCase()) {
-                        //console.log(functionRun);
-                        points++;
-                        if(data[rand].points < questionNum){
-                            data[rand].points++;
-                        }
-                        pop();
-                        
-                        let idName = "el" + (rand+1);
-                        let AidName = idName + "a"
-                        //console.log(idName)
-                        //console.log(data[rand].points + " On..." + (rand + 1));
-                        let element = document.getElementById(idName);
-                        element.style.visibility = "visible";
-                        document.getElementById(AidName).style.visibility = "hidden";
-                        `<b> You have ${points} point(s)!</b>`;
-                        document.getElementById("result").innerHTML = rightMessage(points);
-                }else{
-                    document.getElementById("result").innerHTML = wrongMessage(answer);
-                    let idName = "el" + (rand+1);
-                        let AidName = idName + "a"
-                        let element = document.getElementById(idName);
-                        element.style.visibility = "hidden";
-                        document.getElementById(AidName).style.visibility = "visible";
-                        //console.log(element.style.visibility)
+                if (answer.toLowerCase() == $("#input").val().toLowerCase()) {
+                    points++
+                    if(data[rand].points < questionNum){
+                        data[rand].points++
+                    }
+                    handleRightAnswer(rand, points)
+                } else {
+                    handleWrongAnswer(rand, points)
                     if(data[rand].points > 0){
-                        points--;
-                        data[rand].points --;
-                        //console.log(data[rand].points);
+                        points--
+                        data[rand].points--
                     }
                 }
-                document.getElementById("input").value = "";
-            }else{
-                if (answer == document.getElementById("input").value) {
-                        //console.log(functionRun);
-                        points++;
-                        if(data[rand].points < questionNum){
-                            data[rand].points++;
-                        }
-                        pop();
-                        let idName = "el" + (rand+1);
-                        let AidName = idName + "a"
-                        //console.log(idName)
-                        //console.log(data[rand].points + " On..." + (rand + 1));
-                        let element = document.getElementById(idName);
-                        element.style.visibility = "visible";
-                        document.getElementById(AidName).style.visibility = "hidden";
-                        document.getElementById("result").innerHTML = rightMessage(points);
-                }else{
-                    document.getElementById("result").innerHTML = wrongMessage(answer);
-                    let idName = "el" + (rand+1);
-                        let AidName = idName + "a"
-                        let element = document.getElementById(idName);
-                        element.style.visibility = "hidden";
-                        document.getElementById(AidName).style.visibility = "visible";
-                        //console.log(element.style.visibility)
+            } else {
+                if (answer == $("#input").val()) {
+                    points++;
+                    if(data[rand].points < questionNum){
+                        data[rand].points++;
+                    }
+                    handleRightAnswer(rand, points)
+                } else {
+                    handleWrongAnswer(rand, points)
                     if(data[rand].points > 0){
-                        points--;
-                        data[rand].points --;
-                        //console.log(data[rand].points);
+                        points--
+                        data[rand].points--
                     }
                 }
-                document.getElementById("input").value = ""; 
             }
+            $("#input").val("")
         }
     }
-    rand = Math.floor(Math.random() * data.length);
-    randQuestionAndPoints = data[rand].questionAndPoints;
-    questionNum = Math.floor(Math.random() * randQuestionAndPoints.length);
-    question = randQuestionAndPoints[questionNum].question;
-    answer = randQuestionAndPoints[questionNum].answer;
-    //console.log(rand + " " + answer);
-    //console.log(questionNum);
-    document.getElementById("question").innerHTML = `${question}`;
-    functionRun++;
+    rand = Math.floor(Math.random() * data.length)
+    randQuestionAndPoints = data[rand].questionAndPoints
+    questionNum = Math.floor(Math.random() * randQuestionAndPoints.length)
+    question = randQuestionAndPoints[questionNum].question
+    answer = randQuestionAndPoints[questionNum].answer
+    $("#question").html(`${question}`)
+    functionRun++
     
 }
